5b853ce65243f1d3edc98ba52c83de7b18756da8
serhey <zf.41@tut.by>
2023-08-21 00:39:01 +0300

    - Реализуйте класс `Channel` в файле `src/channel.py` - OK - Реализуйте метод `print_info()` класса `Channel` - OK

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,17 @@
+import json
+import os
+
+from googleapiclient.discovery import build
+
+# YT_API_KEY скопирован из гугла и вставлен в переменные окружения
+api_key: str = os.getenv('YT_API_KEY')
+# специальный объект для работы с API
+youtube = build('youtube', 'v3', developerKey=api_key)
+
+
+def printj(dict_to_print: dict) -> None:
+    """Выводит словарь в json-подобном удобном формате с отступами"""
+    print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
 
 
 class Channel:
@@ -5,8 +19,9 @@ class Channel:
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        channel = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        printj(channel)
